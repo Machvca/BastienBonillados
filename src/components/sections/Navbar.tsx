64bf28d0800805f5305deb93e9720041b2675f1b
Jorge Machuca <jjorgemachuca@gmail.com>
2025-05-09 20:55:17 +0200

    burguer menu

--- src/components/sections/Navbar.tsx
+++ src/components/sections/Navbar.tsx
@@ -13,81 +13,82 @@ function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
   const [isScrolled, setIsScrolled] = useState(false);
   const t = useTranslations();
-  // Detectar el scroll
+
   useEffect(() => {
     const handleScroll = () => {
-      setIsScrolled(window.scrollY > 30); // Cambia cuando baja más de 50px
+      setIsScrolled(window.scrollY > 30);
     };
-
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   return (
-    <nav
-      className={`fixed  font-rubik mx-24 rounded-4xl top-0 left-0 right-0 z-50  bg-clip-text transition-colors duration-600 ${
-        isScrolled ? "bg-stone-900/20 shadow-xl" : "bg-transparent"
-      }`}
-    >
-      <div className="container mx-auto px-4 py-4 flex justify-between items-center">
-        {/* Logo */}
-        <Link href="/">
-          <Image
-            src={logoBastien}
-            alt="Logo"
-            className=" w-10 h-12 cursor-pointer"
-          />
-        </Link>
-
-        {/* Botón de menú hamburguesa */}
-        <button
-          className="md:hidden z-50 fixed top-4 right-4"
-          onClick={() => setIsOpen(!isOpen)}
-        >
-          {isOpen ? (
-            <X size={40} className="text-stone-400" />
-          ) : (
-            <Menu size={40} className="text-[#621316]" />
-          )}
-        </button>
-
-        {/* Menú de navegación (escritorio) */}
-        <div className="hidden md:flex space-x-5 text-2xl ">
-          <Link
-            href="/about"
-            className="text-[#621316] hover:text-[#621316]/60"
-          >
-            {t("ABOUT")}
-          </Link>
-          <Link
-            href="/contact"
-            className="text-[#621316] hover:text-[#621316]/60 "
-          >
-            {t("CONTACT")}
+    <>
+      {/* NAVBAR DESKTOP */}
+      <nav
+        className={`fixed top-0 left-0 right-0 z-50 font-rubik transition-colors duration-600 ${
+          isScrolled ? "bg-stone-900/20 shadow-xl" : "bg-transparent"
+        }`}
+      >
+        <div className="hidden md:flex container mx-auto px-4 py-4 justify-between items-center">
+          {/* Logo */}
+          <Link href="/">
+            <Image
+              src={logoBastien}
+              alt="Logo"
+              className="w-10 h-12 cursor-pointer"
+            />
           </Link>
 
-          <a
-            href="https://www.instagram.com/thetintinmickey/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="hover:text-stone-900-700 cursor-pointer"
-          >
-            <Insta stroke="none" fill="#621316" />
-          </a>
-          <LanguageSwitcher />
+          {/* Links */}
+          <div className="flex space-x-5 text-2xl">
+            <Link
+              href="/about"
+              className="text-[#621316] hover:text-[#621316]/60"
+            >
+              {t("ABOUT")}
+            </Link>
+            <Link
+              href="/contact"
+              className="text-[#621316] hover:text-[#621316]/60"
+            >
+              {t("CONTACT")}
+            </Link>
+            <a
+              href="https://www.instagram.com/thetintinmickey/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-stone-900-700 cursor-pointer"
+            >
+              <Insta stroke="none" fill="#621316" />
+            </a>
+            <LanguageSwitcher />
+          </div>
         </div>
-      </div>
+      </nav>
 
-      {/* Menú móvil */}
+      {/* BOTÓN HAMBURGUESA - MÓVIL */}
+      <button
+        className="md:hidden z-50 fixed top-4 right-4"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {isOpen ? (
+          <X size={40} className="text-stone-400" />
+        ) : (
+          <Menu size={40} className="text-[#621316]" />
+        )}
+      </button>
+
+      {/* MENÚ MÓVIL */}
       <div
-        className={`md:hidden fixed top-0  w-full h-full text-stone-300 bg-linear-to-br from-[#621316] via-[#621316] to-[#621316]  flex flex-col items-center justify-center gap-6 text-xl transition-all duration-700 ease-in-out ${
+        className={`md:hidden fixed inset-0 z-40 text-stone-300 bg-gradient-to-br from-[#621316] via-[#621316] to-[#621316] flex flex-col items-center justify-center gap-10 text-2xl transition-all duration-700 ease-in-out ${
           isOpen
             ? "translate-x-0 opacity-100 pointer-events-auto"
             : "-translate-x-full opacity-0 pointer-events-none"
         }`}
       >
         <BackgroundLines>
-          <div className="flex flex-col items-center justify-center gap-6 text-5xl mt-92 absolute ml-32">
+          <div className="flex flex-col items-center justify-center gap-8 text-4xl pt-72 font-rubik">
             <Link
               href="/about"
               className="hover:text-stone-200"
@@ -115,7 +116,7 @@ function Navbar() {
           </div>
         </BackgroundLines>
       </div>
-    </nav>
+    </>
   );
 }
 
